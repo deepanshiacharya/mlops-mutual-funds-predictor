365e09468b6b6024726ab50292656cf903cb4827
deepanshiacharya <deepanshi.acharya@gmail.com>
2025-10-05 21:21:56 +0530

    Updated app.py

--- src/app.py
+++ src/app.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from src.predict import predict
+from .predict import predict
 
 app = FastAPI(title="Mutual Funds Return Predictor", version="1.0.0")
 
